14e3cf04931bdc3daea8a54b54e785294b8a5ba6
namlose <ahbehnomics@gmail.com>
2020-07-21 19:24:59 +0100

    modified:   hello.class 	modified:   hello.java 	new file:   hellojavaframe.java

---
+++ learningjava/hellojavaframe.java
@@ -0,0 +1,7 @@
+public class hellojavaframe{
+  public static void main(String [] args) {
+    JFrame frame = new JFrame("Hello, JFrame!");
+    frame.setSize( 300, 300);
+    frame.setVisible( true );
+  }
+}
